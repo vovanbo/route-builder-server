00c147518998e5cdfe12c15ef2dec79a68978c38
Vladimir Bolshakov <bo@macbook.local>
2016-06-27 11:00:44 +0300

    Improved error handling is base request handler. Support for send_error and Tornado's web.exceptions.

--- handlers/base.py
+++ handlers/base.py
@@ -1,8 +1,18 @@
-from tornado.escape import json_encode
-from tornado.web import RequestHandler
+from marshmallow import Schema, fields
+from tornado import web, escape
 
 
-class BaseHandler(RequestHandler):
+class JSONAPIErrorSchema(Schema):
+    status = fields.Integer(required=True)
+    source = fields.Dict()
+    detail = fields.Raw()
+
+
+class JSONAPIErrorsSchema(Schema):
+    errors = fields.Nested(JSONAPIErrorSchema, many=True)
+
+
+class BaseHandler(web.RequestHandler):
     @property
     def db(self):
         return self.application.db
@@ -12,12 +22,32 @@ class BaseHandler(RequestHandler):
         return self.application.googlemaps
 
     def set_default_headers(self):
-        self.set_header('Content-Type', 'application/json')
+        self.set_header('Content-Type', 'application/vnd.api+json')
 
     def write_error(self, status_code, **kwargs):
-        result = {
-            'code': status_code,
-            'message': kwargs.get('message'),
-            'errors': kwargs.get('errors', self._reason),
-        }
-        self.finish(json_encode(result))
+        schema = JSONAPIErrorsSchema()
+        result = {'errors': []}
+        if 'exc_info' in kwargs:
+            etype, value, traceback = kwargs['exc_info']
+            for messages_attribute in ['messages', 'args']:
+                messages = getattr(value, messages_attribute, None)
+                if messages:
+                    if isinstance(messages, dict):
+                        result['errors'].append({
+                            'status': status_code,
+                            'detail': messages
+                        })
+                    else:
+                        for message in messages:
+                            result['errors'].append({
+                                'status': status_code,
+                                'detail': message
+                            })
+        elif 'message' in kwargs:
+            result['errors'].append({
+                'status': status_code,
+                'detail': kwargs['message']
+            })
+        if result['errors']:
+            output = schema.dumps(result)
+            self.finish(output.data)
