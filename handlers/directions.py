e7ae5fab17924ccb1a703604e766d4a528c95a39
Vladimir Bolshakov <bo@macbook.local>
2016-06-27 11:50:56 +0300

    Improved error handling of Google Maps API requests in directions handler. Output JSON-API schema for directions is added.

--- handlers/directions.py
+++ handlers/directions.py
@@ -1,26 +1,60 @@
-from marshmallow import Schema, fields, validate
-from tornado import escape
+import googlemaps
+from marshmallow import Schema, validate
+from marshmallow_jsonapi import Schema as JSONAPISchema, fields
 from webargs.tornadoparser import parser
 
+from core.google import ApiErrorCode
 from handlers.base import BaseHandler
 
 
 class DirectionsQuerySchema(Schema):
-    origin = fields.Str(required=True)
-    destination = fields.Str(required=True)
+    origin = fields.Str(required=True, validate=lambda s: bool(s))
+    destination = fields.Str(required=True, validate=lambda s: bool(s))
     waypoints = fields.List(fields.Str)
-    mode = fields.Str(
-        default='driving', validate=validate.OneOf(['driving', 'walking',
-                                                    'bicycling', 'transit'])
-    )
+    mode = fields.Str(default='driving',
+                      validate=validate.OneOf(['driving', 'walking',
+                                               'bicycling', 'transit']))
     language = fields.Str(default='ru')
 
     class Meta:
         strict = True
 
 
+class DirectionsSchema(JSONAPISchema):
+    id = fields.String()
+    route = fields.Dict()
+
+    class Meta:
+        type_ = 'directions'
+        strict = True
+
+
 class DirectionsHandler(BaseHandler):
     async def get(self, *args, **kwargs):
-        args = parser.parse(DirectionsQuerySchema, self.request)
-        routes = await self.googlemaps.directions(**args)
-        self.finish(escape.json_encode(routes))
+        args = parser.parse(DirectionsQuerySchema, self.request,
+                            locations=('query',))
+
+        error_msg = 'Google Maps API error response: %s'
+        try:
+            routes = await self.googlemaps.directions(**args)
+        except googlemaps.exceptions.ApiError as e:
+            self.send_error(
+                ApiErrorCode[e.status].value,
+                message=error_msg % (e.message if e.message else e.status)
+            )
+            return
+        except googlemaps.exceptions.HTTPError as e:
+            self.send_error(e.status_code,
+                            message=error_msg % 'HTTP error')
+            return
+        except googlemaps.exceptions.Timeout as e:
+            self.send_error(599, message=error_msg % 'timeout')
+            return
+        except googlemaps.exceptions.TransportError as e:
+            self.send_error(500, message=error_msg % 'transport error')
+            return
+
+        result = [{'id': i, 'route': r} for i, r in enumerate(routes)]
+        schema = DirectionsSchema(many=True)
+        output = schema.dumps(result)
+        self.finish(output.data)
